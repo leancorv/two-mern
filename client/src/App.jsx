f3281b263d76af0dd0c9372c8a484ee2f928cf40
Leandro <lecorvalan2@gmail.com>
2022-06-01 00:52:06 -0300

    Corrección en formInput.scss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import "./app.scss"
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
+// import Watch from "./pages/watch/Watch";
+import Play from "./pages/play/Play";
+import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
+// import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Register/> ;
+  return <Home />;
 };
 
 export default App;
